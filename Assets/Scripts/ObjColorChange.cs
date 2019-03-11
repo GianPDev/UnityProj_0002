1cebfee90465b95e137360b63b591e995ee432f6
Gian P <gianpdev@gmail.com>
2019-03-12 07:57:48 +1100

    Initial Commit
    Added script to change colors.
    Added script to use other scripts.

---
+++ Assets/Scripts/ObjColorChange.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjColorChange : MonoBehaviour {
+	public ObjColorChange()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log(gameObject.name + " color set to" + setColor(Color.red));
+            //gameObject.GetComponent<Renderer>().material.color = ""; //Find a way to set color by hex
+        }
+	    if(Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log(gameObject.name + " color set to" + setColor(Color.green));
+        }
+	    if(Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log(gameObject.name + " color set to" + setColor(Color.blue));
+        }
+	}
+
+	public Color setColor(Color color)
+	{
+		gameObject.GetComponent<Renderer>().material.color = color;
+		return color;
+	}
+
+	static public Color addRGB(Color color, float red, float green, float blue)
+	{
+		Color _color = color;
+		
+		_color.r += red;
+		_color.g += green;
+		_color.b += blue;
+
+		return _color;
+	}
+}
---
+++ Assets/Scripts/UsingOtherComponents.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingOtherComponents : MonoBehaviour {
+
+	public GameObject otherGameObject;
+	private ObjColorChange objColorChange;
+	private ObjColorChange otherObjColorChange;
+	[SerializeField]private Color color;
+
+	public UsingOtherComponents(Color Color)
+	{
+		color = Color;
+	}
+
+	public UsingOtherComponents()
+	{
+		color = Color.red;
+	}
+	void Awake()
+	{
+		objColorChange = GetComponent<ObjColorChange>();
+		Debug.Log(objColorChange.name + " attached to " + this.name);
+		otherObjColorChange = otherGameObject.GetComponent<ObjColorChange>();
+		Debug.Log(otherObjColorChange.name + " attached to " + this.name);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log(this.name + " Color: " + objColorChange.setColor(color));
+		color = ObjColorChange.addRGB(color, 0.5f, 0.5f, 0.5f);
+		Debug.Log(otherObjColorChange.name + " Color: " + otherObjColorChange.setColor(color));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
